ab2842e3c05c201e99939e8744274720d889bcde
James Lesneski <jlesneski@lucidiait.com>
2022-01-18 22:53:37 -0500

    initial load

---
+++ Containers/python/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import os
+
+vers = os.getenv('CODEVERSION')
+if not vers:
+    vers = "Local"
+desc = "Test Python App"
+
+app = FastAPI(title="Container Workshop", description=desc, version=vers)
+
+@app.get("/")
+def read_root():
+    return {"LucidiaIt": "Rocks"}
